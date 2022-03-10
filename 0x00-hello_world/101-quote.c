3181cca37a203a4cf03a8a609918d82447409676
Motari <brianmotari3@gmail.com>
2022-03-10 03:13:15 -0800

    C hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
-#include<stdio.h>
-#include<string.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - main function
  * Description: prints a quote
@@ -8,8 +7,6 @@
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
-	write(2, ""and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,s");
-	fprintf(stderr, "%s", "\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
