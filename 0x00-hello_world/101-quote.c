6d464b347f1778e8463c133a5b48aa11cd6a5e03
Motari <brianmotari3@gmail.com>
2022-03-10 01:15:15 -0800

    C hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+/**
+ * main - main function
+ * Description: prints a quote
+ * Return: 1
+ */
+int main(void)
+{
+	int s = strlen("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	write(2, ""and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,s");
+	fprintf(stderr, "%s", "\n");
+	return (1);
+}
